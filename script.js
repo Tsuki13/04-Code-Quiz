b57859308e090291e91ccf32697a571276aacb03
Rachel Diaz <Rachel7113@gmail.com>
2021-01-18 12:01:28 -0500

    Updated CSS, added timer & questions to JS

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 var startBtn = document.getElementById("start-btn");
+var nextBtn = document.getElementById('next-btn')
 var questionContainer = document.getElementById("question-container");
 var questionTextEl = document.getElementById('question');
 var answerTextEl = document.getElementById('answer-buttons');
+var startingMin = 1;
+let time = 1 * 60;
+var timerElement = document.getElementById('timerDisplay');
 
-var randomQuestions, questionNum
+let randomQuestions, questionNum
 
-startBtn.addEventListener("click", startQuiz);
+let score = 0;
+
+startBtn.addEventListener('click', startQuiz);
+nextBtn.addEventListener('click', () => {
+    questionNum++
+    setNextQuestion()
+})
+
+var timer = setInterval(updateTimer, 1000);
+
+    
+function updateTimer(){
+    var minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    timerElement.innerHTML = minutes+ ':' +seconds;
+    time --;
+    if (time == -2) {
+        alert('You are out of time');
+        clearInterval(timer);
+        startBtn.innerText = 'Restart'
+        startBtn.classList.remove('hide')
+        return
+    }
+} 
 
 function startQuiz() {
     var nextBtn = document.getElementById("next-btn");
     questionContainer.classList.remove("hide");
     nextBtn.classList.remove("hide");
     startBtn.classList.add("hide");
+    timerDisplay.classList.remove("hide");
     randomQuestions = myQuestions.sort(() => Math.random() - .5);
     questionNum = 0;
     setNextQuestion()
@@ -20,16 +48,69 @@ function startQuiz() {
 }
 
 function setNextQuestion () {
+    resetState()
     showQuestion(randomQuestions[questionNum]);
 }
 
 function showQuestion(question) {
-    questionTextEl.innerText = question.question 
+    questionTextEl.innerText = question.question
+    question.answers.forEach(answer => {
+    var button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerTextEl.appendChild(button)
+    });
  }
-
-function selectAnswer() {
-
+function resetState() {
+    nextBtn.classList.add('hide')
+    while (answerTextEl.firstChild){
+        answerTextEl.removeChild
+        (answerTextEl.firstChild)
+    }
 }
+
+function selectAnswer(e) {
+    var selectedBtn = e.target
+    var correct = selectedBtn.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerTextEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (randomQuestions.length > questionNum + 1) {
+        nextBtn.classList.remove('hide')
+    } else {
+        startBtn.innerText = 'Restart'
+        startBtn.classList.remove('hide')
+    }
+    
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+// function answerIsCorrect() {
+//     document.getElementById(answerTextEl).style.backgroundColor = "green"
+
+// }
+
+// function answerIsWrong() {
+//     document.getElementById(answerTextEl).style.backgroundColor = "red"
+// }
 
 var myQuestions = [
     {
@@ -39,6 +120,42 @@ var myQuestions = [
         {text: 'A method to join strings.', correct: false},
         {text: 'Allows you to make a decision based on a condition.', correct: false},
         {text: 'Allows you to store information so it can be reused throughout the program', correct: true}
+    ]
+    },
+    {
+     question: 'Which is the best description of concatenation?',
+     answers: [
+        {text: 'Identifies a portion of a string.', correct: false},
+        {text: 'A method to join strings.', correct: true},
+        {text: 'Allows you to make a decision based on a condition.', correct: false},
+        {text: 'Allows you to store information so it can be reused throughout the program', correct: false}
+     ]  
+    },
+    {
+    question: 'Which is the best description of an if statement?',
+    answers: [
+        {text: 'Identifies a portion of a string.', correct: false},
+        {text: 'A method to join strings.', correct: false},
+        {text: 'Allows you to make a decision based on a condition.', correct: true},
+        {text: 'Allows you to store information so it can be reused throughout the program', correct: false}
+    ]
+    },
+    {
+    question: 'what does the "typeof(3);" expression return?',
+    answers: [
+        {text: 'undefined', correct: false},
+        {text: 'string', correct: false},
+        {text: 'number', correct: true},
+        {text: 'boolean', correct: false}
+    ]
+    },
+    {
+    question: 'what does the "typeof(3) === typeof(4.32);" expression return?',
+    answers: [
+        {text: 'undefined', correct: false},
+        {text: 'false', correct: false},
+        {text: 'true', correct: true},
+        {text: 'null', correct: false}
     ]
     }
 ]
